1efb3dd01e1b7453a60a4e90a6809a2654fcbdff
reddevilcero <reddevil_cero@hotmail.com>
2020-02-05 12:21:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,13 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+ header = doc.css('.headline-26OIBN')
+online_course = doc.css('.inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC')
+# online_course.each do |course|
+#   puts course.text.strip
+# end
+
+p online_course[0].children
