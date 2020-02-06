cb42400bdf7778c4498fc71d5932b37b8a0358ae
Brian Kleiner <brian.j.kleiner@gmail.com>
2020-02-06 05:22:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
+# Nokogiri::HTML(html)
+# doc = Nokogiri::HTML(html)
+# puts doc
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+
+puts doc.css(".title-3Kf9MY").text
