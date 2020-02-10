6bd5561ac2c8baea735dc70f17f2c4f3d94daa5f
Khalid T <khalid.tissire@gmail.com>
2020-02-10 06:16:32 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,19 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 
- header = doc.css('.headline-26OIBN')
+header = doc.css('.headline-26OIBN')
+puts header.text.strip
+
+puts "Courses: "
+4.times { |i| puts doc.search(".title-oE5vT4")[i].text.strip }
+
+
+
+=begin
+
 online_course = doc.css('.text-4GLMvr.title-oE5vT4')
 online_course.each do |course|
   puts course.text.strip
 end
+
+=end
